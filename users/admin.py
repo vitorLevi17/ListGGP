1097403863493876b721197f0dc2f500d077ae17
vitorLevi17 <85260420+vitorLevi17@users.noreply.github.com>
2026-02-25 17:00:03 -0300

    feat: Login, ainda incompleto

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.models import User
 from django.contrib import admin
 
-# Register your models here.
-class ListGGPAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'description')
-    search_fields = ('nome')
+# Certifique-se de apagar aquela linha "from .models import ListGGP" se ela ainda estiver aí!
 
-admin.site.register(ListGGP, ListGGPAdmin)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+
+# 1. Desregistra o User padrão do Django
+admin.site.unregister(User)
+
+# 2. Registra o User novamente, mas agora usando a sua configuração
+admin.site.register(User, UserAdmin)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from users.forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            if User.objects.filter(username=form.cleaned_data['username']).exists():
+                user = authenticate(request, username=form.cleaned_data['username'],
+                                     password=form.cleaned_data['password'])
+                if user is not None:
+                    login(request, user)
+                    return redirect('index')
+                else:
+                    form.add_error(None, 'Usuário ou senha incorretos.')            
+    else:
+        form = LoginForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'user/login.html', context)
