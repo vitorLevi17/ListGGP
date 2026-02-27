488b55643b208153f40e16ebae77f25da6fa8a67
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-02-27 10:13:19 -0300

    feat: Adicionar logout

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from users.views import login_view,index,usuario_altera_senha
+from users.views import login_view,index,usuario_altera_senha,logout
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('index/',index,name='index'),
-    path('alterar_senha',usuario_altera_senha,name='usuario_altera_senha')
+    path('alterar_senha',usuario_altera_senha,name='usuario_altera_senha'),
+    path('logout/',logout,name='logout')
     ]
