d7d827393f04d77f1ea0380b831f0a0dc5ccd076
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-03-03 10:12:03 -0300

    feat: Visualizar eventos marcados

--- events/urls.py
+++ events/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
+from .views import listar_treinamentos_marcados
 
-#urlpatterns = [
-#    path('cadastrar-evento/',nome-da-view(name='events-cadastrar-evento'))   
-#]
+urlpatterns = [
+    path('listar-eventos-marcados/',listar_treinamentos_marcados,name='listar-eventos-marcados'),
+    #path('cadastrar-evento/',nome-da-view(name='events-cadastrar-evento'))   
+
+]
--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Treinamento
 
-# Create your views here.
+@login_required(login_url='/login')
+def listar_treinamentos_marcados(request):
+    lista_treinamentos = Treinamento.objects.filter(status='MARCADO')
+    return render(request,'events/listar-treinamento.html',{'treinamentos':lista_treinamentos})
