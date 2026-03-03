26bb7f7b649db5b1758589c45ca780a0b580d6a7
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-03-03 07:35:32 -0300

    raw: Modelagem da entidade treinamento

--- events/admin.py
+++ events/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
-from .models import Aula
+from .models import Aula,Treinamento
 
 class ListAula(admin.ModelAdmin):
     list_display = ("nm_aula", 
     "descricao",
     "palestrante",
     "carga_horaria")
+class ListTreinamento(admin.ModelAdmin):
+    list_display = ("nm_evento",
+                    "data",
+                    "local",
+                    "status"
+                    )
+    filter_horizontal = ('aulas',)
 
 admin.site.register(Aula,ListAula)
+admin.site.register(Treinamento,ListTreinamento)
--- events/models.py
+++ events/models.py
@@ -1,14 +1,32 @@
 from django.db import models
 
-# Create your models here.
-#AULAS
-#SETORES
-#TREINAMENTOS
 class Aula(models.Model):
     nm_aula = models.CharField(max_length=150)
     descricao = models.CharField(max_length=200)
     palestrante = models.CharField(max_length=100)
     carga_horaria = models.IntegerField(null=False)
-
     
+    def __str__(self):
+        return self.nm_aula
 
+class Treinamento(models.Model):
+
+    opcoes_local = [
+        ("AUDITORIO HSI","hsi"),
+        ("AUDITORIO ADM CENTRAL","pupileira"),
+    ]
+    opcoes_status = [
+        ("FINALIZADO","finalizado"),
+        ("CANCELADO","cancelado"),
+        ("MARCADO","marcado"),
+    ]
+
+    nm_evento = models.CharField(max_length=100)
+    data = models.DateField()
+    local = models.CharField(max_length=50,choices=opcoes_local)
+    status = models.CharField(max_length=50,choices=opcoes_status)
+    participantes = models.JSONField(default=list,blank=True)
+    aulas = models.ManyToManyField(Aula, related_name='aulas')
+
+    def __str__(self):
+        return self.nm_evento
