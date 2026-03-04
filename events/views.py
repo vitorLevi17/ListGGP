a0eeb20f57bfb2628677dda2dfa705397179a635
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-03-04 11:59:02 -0300

    feat: Ler QR code e inserir participantes na lista

--- events/views.py
+++ events/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .models import Treinamento
+from django.contrib import messages
 
 @login_required(login_url='/login')
 def listar_treinamentos_marcados(request):
@@ -15,6 +16,18 @@ def conferir_treinamento(request,treinamento_id):
 def iniciar_treinamento(request,treinamento_id):
     treinamento = Treinamento.objects.get(id=treinamento_id)
     treinamento.status = "ANDAMENTO"
+    treinamento.save()
+    return redirect('conferir-treinamento',treinamento_id=treinamento.id)
+
+@login_required(login_url='/login')
+def adicionar_participante(request,treinamento_id,matricula_participante):
+    treinamento = Treinamento.objects.get(id=treinamento_id)
+    
+    if(matricula_participante in treinamento.participantes):
+        messages.warning(request, f'Atenção: A matrícula {matricula_participante} já foi registrada neste evento!')
+        return redirect('conferir-treinamento',treinamento_id=treinamento.id)
+
+    treinamento.participantes.append(matricula_participante)
     treinamento.save()
     return redirect('conferir-treinamento',treinamento_id=treinamento.id)
     
