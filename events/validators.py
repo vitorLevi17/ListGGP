e2626816b08275037fc7871f9f28440db7a021d2
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-04-07 11:45:09 -0300

    refactor: Adicionar classe especifica para as validações

---
+++ events/validators.py
@@ -0,0 +1,39 @@
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.utils import timezone
+from datetime import datetime
+
+def validar_permissoes_usuario(request,treinamento,mensagem_erro):
+    if request.user != treinamento.usuario_cadastrante and not request.user.groups.filter(name='RH GESTOR').exists():
+        messages.warning(request,mensagem_erro)
+        return redirect('conferir-treinamento',treinamento_id=treinamento.id)
+    else:
+        return None
+    
+def validar_horario_inicio_treinamento(request,treinamento):
+    limite_inicio = treinamento.data - timezone.timedelta(minutes=30)
+    if timezone.now() < limite_inicio:
+        messages.warning(request,f'Atenção: O treinamento só pode ser iniciado faltando 30 minutos para o horário marcado.')
+        return redirect('conferir-treinamento', treinamento_id=treinamento.id)
+    return None
+
+def validar_horario_fim_treinamento(request,treinamento):
+    data_treinamento = timezone.localtime(treinamento.data).date()
+    data_atual = datetime.combine(data_treinamento, treinamento.horario_final)
+    momento_termino = timezone.make_aware(data_atual)
+    
+    if timezone.now() < momento_termino:
+        messages.warning(request,f'Atenção: O treinamento só pode ser finalizado após o horário marcado.')
+        return redirect('conferir-treinamento', treinamento_id=treinamento.id)
+    return None
+
+def validar_horario_fim_treinamento_cadastro(request,treinamento):
+    data_inicio_treinamento = timezone.localtime(treinamento.data)
+    
+    horario_fim_sem_fuso = datetime.combine(data_inicio_treinamento.date(), treinamento.horario_final)
+    horario_fim_completo = timezone.make_aware(horario_fim_sem_fuso)
+    
+    if data_inicio_treinamento > horario_fim_completo or treinamento.data < timezone.now():
+        messages.warning(request,f'Atenção: As datas e horários de inicio e término estão inválidos.')
+        return True
+    return False   
--- events/views.py
+++ events/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from .forms import CriarEventoForm,CriarAulaForm
 from django.utils import timezone
 from datetime import datetime
-
+from .validators import *
 
 @login_required(login_url='/login')
 def listar_treinamentos_marcados(request):
@@ -98,7 +98,6 @@ def criar_evento(request):
         form = CriarEventoForm(request.POST)
 
         if form.is_valid():
-
     
             erro_horario = validar_horario_fim_treinamento_cadastro(request, form.instance)
             if erro_horario:
@@ -209,38 +208,3 @@ def remover_participante(request,treinamento_id,matricula_participante):
         messages.warning(request, f'Atenção: A matrícula {matricula_participante} não foi registrada neste evento!')
          
     return redirect('conferir-treinamento',treinamento_id=treinamento.id)
-
-def validar_permissoes_usuario(request,treinamento,mensagem_erro):
-    if request.user != treinamento.usuario_cadastrante and not request.user.groups.filter(name='RH GESTOR').exists():
-        messages.warning(request,mensagem_erro)
-        return redirect('conferir-treinamento',treinamento_id=treinamento.id)
-    else:
-        return None
-    
-def validar_horario_inicio_treinamento(request,treinamento):
-    limite_inicio = treinamento.data - timezone.timedelta(minutes=30)
-    if timezone.now() < limite_inicio:
-        messages.warning(request,f'Atenção: O treinamento só pode ser iniciado faltando 30 minutos para o horário marcado.')
-        return redirect('conferir-treinamento', treinamento_id=treinamento.id)
-    return None
-
-def validar_horario_fim_treinamento(request,treinamento):
-    data_treinamento = timezone.localtime(treinamento.data).date()
-    data_atual = datetime.combine(data_treinamento, treinamento.horario_final)
-    momento_termino = timezone.make_aware(data_atual)
-    
-    if timezone.now() < momento_termino:
-        messages.warning(request,f'Atenção: O treinamento só pode ser finalizado após o horário marcado.')
-        return redirect('conferir-treinamento', treinamento_id=treinamento.id)
-    return None
-
-def validar_horario_fim_treinamento_cadastro(request,treinamento):
-    data_inicio_treinamento = timezone.localtime(treinamento.data)
-    
-    horario_fim_sem_fuso = datetime.combine(data_inicio_treinamento.date(), treinamento.horario_final)
-    horario_fim_completo = timezone.make_aware(horario_fim_sem_fuso)
-    
-    if data_inicio_treinamento > horario_fim_completo or treinamento.data < timezone.now():
-        messages.warning(request,f'Atenção: As datas e horários de inicio e término estão inválidos.')
-        return True
-    return False   
