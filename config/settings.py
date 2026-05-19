76bbe9b6235930bf18daf2d659e63a5450940c80
Levi Santos <85260420+vitorLevi17@users.noreply.github.com>
2026-05-19 11:14:33 -0300

    config: Configurações de segurança de cookies

--- config/settings.py
+++ config/settings.py
@@ -138,3 +138,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Ativa a compressão do WhiteNoise (deixa o carregamento super rápido)
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+SECURE_SSL_REDIRECT = True
